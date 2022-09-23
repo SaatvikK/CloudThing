8681b40e54aeea49276a5f4d1713df90a10d36ca
SaatvikK <skambhampati24@gmail.com>
2022-09-23 12:34:41 +0100

    installed mongodb drivers

--- APIStuff/handlers.go
+++ APIStuff/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,4 +11,20 @@ func login(res http.ResponseWriter, req *http.Request) {
 	PathParams := mux.Vars(req) // Getting the parameters of the URL as a dictionary.
 	res.Header().Set("Content-Type", "application/json")
 	username, ok := PathParams["username"]
+	if !ok {
+		fmt.Println("USERNAME ERR")
+		fmt.Println(ok)
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte(`{ "result": false, "reason": "Username error.", "error": nil, "data": nill }`))
+		return
+	}
+
+	password, ok := PathParams["pwd"]
+	if !ok {
+		fmt.Println("USERNAME ERR")
+		fmt.Println(ok)
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte(`{ "result": false, "reason": "Password error.", "error": nil, "data": nill }`))
+	}
+
 }
