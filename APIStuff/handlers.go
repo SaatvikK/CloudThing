555e2e90b17e7f04e0e05a9845f56107c4c78206
SaatvikK <skambhampati24@gmail.com>
2022-09-23 23:21:35 +0100

    learning mongo and golang

--- APIStuff/handlers.go
+++ APIStuff/handlers.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"reflect"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+// HELPER FUNCS --------------------------------------------------------------
+func conn() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println(reflect.TypeOf(client))
+	return client
+}
+
+func checkReadDB(email string, pwd string) {
+	client := conn()
+	collection := client.Database("Main").Collection("users")
+	var result bson.M
+	err := collection.FindOne(context.TODO(), bson.D{{"email", email}}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// This error means your query did not match any documents.
+			return
+		}
+		panic(err)
+	}
+	fmt.Println(result)
+}
 
 func login(res http.ResponseWriter, req *http.Request) {
 	PathParams := mux.Vars(req) // Getting the parameters of the URL as a dictionary.
---
+++ mongoapp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func conn() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println(reflect.TypeOf(client))
+	return client
+}
+
+func checkReadDB(email string, pwd string) {
+	client := conn()
+	collection := client.Database("Main").Collection("users")
+	var result bson.M
+	err := collection.FindOne(context.TODO(), bson.D{{Key: "email", Value: email}}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// This error means your query did not match any documents.
+			return
+		}
+		panic(err)
+	}
+	fmt.Println(result)
+}
+
+func main() {
+	checkReadDB("saatvikrk@gmail.com", "HI")
+}
